341015cfc7c8c52178aef93991375e63d4143a5b
ember-code <knandor407@gmail.com>
2021-08-05 10:46:48 +0200

    tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+let c=0;
+let f=0;
+const ftoc = function(fp) {
+  c=(fp-32)*(5/9);
+  c=(Math.round(c * 10) / 10);
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(cp) {
+f=cp*(9/5)+32;
+f=(Math.round(f * 10) / 10);
+return f;
 };
 
 module.exports = {
