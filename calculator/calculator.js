0a81e6441f6235343e5f7018b456462ae1010091
ember-code <knandor407@gmail.com>
2021-08-06 21:08:53 +0200

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum1=array.reduce((total,number)=>{
+    total+=number;
+    return total;
+  },0);
+  return sum1;
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  const multipled=array1.reduce((a,m)=>{
+    a*=m;
+    return a;
+  },1);
+  return multipled;
 };
 
-const power = function() {
-	
+const power = function(num,to) {
+	return Math.pow(num,to);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let factorial=1;
+	if(n===0) return 1;
+  for(let i=n;i>=1;i--){
+    factorial*=i;
+  }
+  return factorial;
 };
 
 module.exports = {
