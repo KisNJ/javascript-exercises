987c78b695f0f86b7c07051d09bde91bae82dcdd
ember-code <knandor407@gmail.com>
2021-08-07 08:58:40 +0200

    fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num=parseInt(num);
+    if(num<0){
+        return "OOPS";
+    }
+    
+    let k=0;
+    let l=1;
+    
+    for(let i=1;i<num;i++){
+      let temp=l;
+      l=l+k;
+      k=temp;
+      
+    };
+    return l;
 };
 
 module.exports = fibonacci;
