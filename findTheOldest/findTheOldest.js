3b9249720e9ce990ec7cf30e51c8800ce70bf00d
ember-code <knandor407@gmail.com>
2021-08-07 09:48:16 +0200

    findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    const oldest=array.reduce((previous,current)=>{
+        let today=new Date();
+        const previousAge=getAge(previous.yearOfBirth,previous.yearOfDeath);
+        const currentAge=getAge(current.yearOfBirth,current.yearOfDeath);
+        return previousAge<currentAge?current:previous;
+    });
+    return oldest;
+    };
+    
+    
+    const getAge = function(birth, death) {
+      if (!death) {
+        death = new Date().getFullYear();
+      }
+      return death - birth;
+    };
 
 module.exports = findTheOldest;
