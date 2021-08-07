25ec7050af9c1b265178ac3012ddb4e3c9b0d640
ember-code <knandor407@gmail.com>
2021-08-07 08:30:30 +0200

    palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const splitted = word.split(" ");
+    let modified = splitted.map((part) => {
+      part = part.replace(",", "");
+      part = part.replace(".", "");
+      part=part.replace("!","");
+      return part;
+    });
 
-};
-
+    modified = modified.join("");
+    //return modified;
+    if (modified.toLowerCase() == Array.from(modified.toLowerCase()).reverse().join("")) {
+      return true;
+    }
+    return false
+  };
 module.exports = palindromes;
